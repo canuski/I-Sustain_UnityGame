a2ae936f5bc538f977c1b7a6ed8d96e3471a4bc3
canuski <125011800+canuski@users.noreply.github.com>
2024-01-05 22:06:49 +0100

    Add Fatih RenderTextures, scripts, shaders

---
+++ Assets/Fatih/Scripts/CheckUpsidedown.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckUpsidedown : MonoBehaviour
+{
+    void Update()
+    {
+        if (Vector3.Dot(transform.up, Vector3.down) > 0)
+        {
+            SceneManager.LoadScene("Level_5");
+        }
+
+    }
+}
