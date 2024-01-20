9a5dd16d106e49d4d14bb6d8ec36402192299044
Haitam <Haitam.baqloul@hotmail.com>
2024-01-20 23:28:55 +0100

    fix game

---
+++ Assets/Haitam/Scripts/ShowLore.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowLore : MonoBehaviour
+{
+    public TextMeshProUGUI displayText;
+    public string textToDisplay = "Dit is mijn tekst!";
+    public float displayDuration = 20f;
+
+    void Start()
+    {
+        displayText.text = textToDisplay;
+
+        Invoke("HideText", displayDuration);
+    }
+
+    void HideText()
+    {
+        displayText.gameObject.SetActive(false);
+    }
+}
--- Assets/Haitam/Scripts/TankStation.cs
+++ Assets/Haitam/Scripts/TankStation.cs
@@ -1,28 +1,93 @@
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TankStationSpawner : MonoBehaviour
 {
-    // Voeg het tankstation-prefab toe vanuit de Unity-editor
     public GameObject tankStationPrefab;
+    public GameObject collectible1Prefab;
+    public GameObject collectible2Prefab;
+    public GameObject collectible3Prefab;
+    private Vector3 tankStationSpawnLocation;
 
-    // Update is called once per frame
-    void Update()
+    private bool tankStationSpawned = false;
+    private List<GameObject> collectedCollectibles = new List<GameObject>();
+
+    public TextMeshProUGUI collectedText;
+
+    void Start()
     {
-        // Controleer of de spatiebalk wordt ingedrukt
-        if (Input.GetKeyDown(KeyCode.Space))
+        GameObject tankStationSpawnPoint = GameObject.Find("TankStationSpawnPoint");
+        if (tankStationSpawnPoint != null)
         {
-            // Roep de functie aan om een tankstation te spawnen op de huidige positie
-            SpawnTankStation();
+            tankStationSpawnLocation = tankStationSpawnPoint.transform.position;
+        }
+        else
+        {
+            Debug.LogError("TankStationSpawnPoint not found. Make sure it exists in the scene.");
         }
     }
 
-    // Functie om een tankstation te spawnen op de huidige positie
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!tankStationSpawned && CheckCollectedCollectibles())
+            {
+                SpawnTankStation();
+            }
+        }
+
+        // Bijwerken van de tekst op het scherm
+        UpdateUIText();
+    }
+
     void SpawnTankStation()
     {
-        // Verkrijg de huidige positie van het script
-        Vector3 spawnPosition = transform.position;
+        Instantiate(tankStationPrefab, tankStationSpawnLocation, Quaternion.identity);
+        tankStationSpawned = true;
+        collectedCollectibles.Clear();
+    }
 
-        // Spawn het tankstation op de gewenste positie
-        Instantiate(tankStationPrefab, spawnPosition, Quaternion.identity);
+    bool CheckCollectedCollectibles()
+    {
+        List<GameObject> requiredCollectiblePrefabs = new List<GameObject> { collectible1Prefab, collectible2Prefab, collectible3Prefab };
+
+        foreach (GameObject requiredCollectiblePrefab in requiredCollectiblePrefabs)
+        {
+            if (!collectedCollectibles.Contains(requiredCollectiblePrefab))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            Collect(other.gameObject);
+        }
+    }
+
+    void Collect(GameObject collectibleGameObject)
+    {
+        if (!collectedCollectibles.Contains(collectibleGameObject))
+        {
+            collectedCollectibles.Add(collectibleGameObject);
+            Destroy(collectibleGameObject);
+        }
+    }
+
+    // Functie om de tekst op het scherm bij te werken
+    void UpdateUIText()
+    {
+        // Bijwerken van de tekst met het aantal verzamelde collectibles
+        if (collectedText != null)
+        {
+            collectedText.text = "Collected collectibles: " + collectedCollectibles.Count+"\nPress T after you have collected 3 collectibles";
+        }
     }
 }
